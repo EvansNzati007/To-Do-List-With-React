41f053289628a2fc5d6a2a401313c7c011715bf5
EvansNzati007 <evansnzati@gmail.com>
2024-10-01 21:26:22 +0100

    quelques changement

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import ToDoList from './ToDoList'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+     <ToDoList></ToDoList>
+    </>
+  )
+}
+
+export default App
---
+++ src/ToDoList.jsx
@@ -0,0 +1,106 @@
+import React ,{useState} from  'react';
+
+function ToDoList(){
+    const [tasks, setTasks] = useState(["Dejeuner", "Diner", "Souper", "Dormir"]);
+    const [newTask, setNewTask] = useState('');
+
+    function handleInputChange(event){
+
+        setNewTask(event.target.value)
+
+    }
+    function addTask(){
+     const  ne = document.getElementById('task').value;
+        if(ne == ""){
+            alert("please enter a task name")
+        }else{
+            setTasks(t => [...t, newTask]);
+            setNewTask("");
+    
+
+        }
+       
+    }
+
+    function removeTask(index){
+       // alert("are you sure you want to remove "+tasks[index]+ "?");
+      
+       if( confirm("Are you sure you want to remove "+tasks[index]+" ?")){
+        setTasks(tasks.filter((_,i) => i!==index));
+       }
+
+           
+       
+
+    }
+    function moveTaskUp(index){
+        if(index >0){
+
+            const upDateTasks = [...tasks];
+            [ upDateTasks[index], upDateTasks[index-1]] = 
+            [upDateTasks[index - 1], upDateTasks[index]];
+            setTasks(upDateTasks);
+
+        }
+       
+
+  
+    }
+    function moveTaskDown(index){
+
+        if(index >=0){
+
+            const upDateTasks = [...tasks];
+            [ upDateTasks[index], upDateTasks[index+1]] =
+            [upDateTasks[index+1], upDateTasks[index]];
+            setTasks(upDateTasks);
+        }
+
+    }
+
+
+    return(
+        <>
+        <div className='todo-list'>
+            <h1>ToDo List</h1>
+            <div>
+                <input type="text"
+                        placeholder='add a task to...'
+                        value={newTask}
+                        id='task'
+                        onChange={handleInputChange}
+                
+                />
+                <button className='add-task'
+                        onClick={addTask}
+                                    >
+                    Add A Task</button>
+            </div>
+
+            <ol>
+            {tasks.map((task, index) => <li key={index}>
+                <span className="text">{task}</span>
+                <button className='delete-btn'
+                        onClick={()=> removeTask(index)}>
+                    Delete
+                </button>
+                <button className='up-btn'
+                        onClick={()=> moveTaskUp(index)}>
+                    Up
+                </button>
+                <button className='down-btn'
+                        onClick={()=> moveTaskDown(index)}>
+                Down
+                </button>
+                 </li>
+                
+                
+            )}
+        </ol>
+        </div>
+        
+        </>
+    );
+}
+
+export default ToDoList;
